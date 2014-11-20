d66990ec787b4039d99c5a98b4b794fb315cac42
MerrittJ <merrittj@aston.ac.uk>
2014-11-20 17:59:24 +0000

    focused on the scanning of an individual book into the hashmap and incrementing the key as it cycles resulting in e.g. genesis:3:65

--- DSCW/src/code/Tokeniser.java
+++ DSCW/src/code/Tokeniser.java
@@ -2,6 +2,8 @@ package code;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import externalCode.*;
@@ -10,9 +12,10 @@ public class Tokeniser {
 
 	private Scanner direcScan;
 	private static final int NUM_OF_BOOKS = 66;
+	private Map<String, String> map;
 	
 	public Tokeniser(){
-		
+		map = new HashMap<String, String>();
 	}
 	
 	public Book[] build() throws FileNotFoundException{
@@ -80,6 +83,37 @@ public class Tokeniser {
 			}
 		}
 		*/
+	
+	public void loadtoo(File file, String bookName) throws FileNotFoundException {
+		Scanner chapSc = new Scanner(file);
+		chapSc.useDelimiter("Chapter");
+		
+		
+		String currentChap = chapSc.next();
+		
+		int chapNum = 1;
+		int verseNum = 1;
+		String verseRef = bookName +":"+ chapNum +":"+ verseNum;
+		
+		while(chapSc.hasNext()){
+			chapSc.next(); // skip 'Book of xxx' section of text on first run
+			chapNum++;
+			Scanner verseSc = new Scanner(currentChap);
+			verseSc.useDelimiter("\n");
+			
+			while (verseSc.hasNext()) {
+				map.put(verseRef, verseSc.next());
+				verseSc.next();
+				verseNum++;
+				verseRef = bookName +":"+ chapNum +":"+ verseNum;
+			}
+			verseSc.close();
+			chapNum++;
+			verseRef = bookName +":"+ chapNum +":"+ verseNum;
+		}
+		chapSc.close();
+			
+	}
 		
 	public static void main(String args[]) throws FileNotFoundException{
 		Tokeniser toke= new Tokeniser();
