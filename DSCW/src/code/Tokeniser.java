351523fd133310e498dac96a5f6fe3d4ef261983
Tom Connolly <tom.connolly2511@gmail.com>
2014-11-28 12:02:11 +0000

    changes to tokeniser. now loadToo method takes one book and a name and returns a hashmap of the verses

--- DSCW/src/code/Tokeniser.java
+++ DSCW/src/code/Tokeniser.java
@@ -2,92 +2,21 @@ package code;
 
 import java.io.*;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
-import externalCode.*;
-import textdocs.*;
 
 public class Tokeniser extends ClassLoader{
 
-	private Scanner direcScan;
-	private static final int NUM_OF_BOOKS = 66;
-	private Map<String, String> map;
+
+	private HashMap<String, String> map;
 	
 	public Tokeniser(){
 		map = new HashMap<String, String>();
 	}
 	
-	public Book[] build() throws FileNotFoundException{
-		//create array of books to hold arrays of chapters
-		Book[] books = new Book[NUM_OF_BOOKS];
-		for (int i = 0; i < NUM_OF_BOOKS; i++){
-			Book book = new Book();
-			books[i] = book;
-		}
-		//create scanner that will scanner
-		direcScan = new Scanner(new File("C://Users/Tom/Documents/Aston/ComputerScience/Y2.T1/CS2310/Labs/BibleApp/bin/textdocs/fileDirec.txt"));
-		
-		while(direcScan.hasNextLine()){
-			
-			String fileName = "C://Users/Tom/Documents/Aston/ComputerScience/Y2.T1/CS2310/Labs/BibleApp/bin/textdocs/".concat(direcScan.nextLine());
-			System.out.println(fileName);
-			
-			Counter counter = new Counter(fileName);
-			
-			//loop to create the correct amount of chapters (counter.getNumOfChapters()) in each book object
-			for(int bookCounter = 1; bookCounter < NUM_OF_BOOKS; bookCounter++){
-				
-				books[bookCounter].createChapters(counter.getNumOfChapters());
-				
-				//loop to create the correct amount of verses (counter.getNumOfVerses(i)) in each chapter object
-				for(int chapterCounter = 1; chapterCounter < counter.getNumOfChapters(); chapterCounter++){
-					
-					books[bookCounter].getChapter(chapterCounter).createVerses(counter.getNumOfVerses(chapterCounter));
-					
-
-				}
-			}
-		}
-		return books;
-	}
-	
-	/*
-	public void load(){
-		
-		Scanner fileDirecScan = new Scanner(new File("BibleApp/KJBible/fileDirec.txt"));
-		fileDirecScan.useDelimiter("\n");
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		
-		while(fileDirecScan.hasNext()){
-			Scanner chapScan = new Scanner(fileDirec.next());
-			chapScan.useDelimiter("Chapter");
-			
-			while(chapScan.hasNext()){
-				Scanner verseScan = new Scanner(chapScan.next());
-				verseScan.useDelimiter("\n");
-				
-				while(verseScan.hasNext()){
-					Scanner wordScan = new Scanner(verseScan.next());
-					wordScan.useDelimiter(" ");
-					
-					while(wordScan.hasNext()){
-						books.getChapter(i).getVerse(k).setWord(l) = wordScan.next();
-						l++;
-					}
-					k++;
-				}
-				j++;
-			}
-		}
-		*/
-	
-	public void loadtoo(File file, String bookName) throws FileNotFoundException {
+	public HashMap<String, String> loadToo(File file, String bookName) throws FileNotFoundException {
 		Scanner chapSc = new Scanner(file);
 		chapSc.useDelimiter("CHAPTER");
-		
 		
 		String currentChap = chapSc.next();
 		System.out.println(currentChap);
@@ -107,8 +36,6 @@ public class Tokeniser extends ClassLoader{
 			
 			while (verseSc.hasNext()) {
 				map.put(verseRef, verseSc.next());
-				//print statement to print each line to console. should be removed later
-				System.out.println(map.get(verseRef));
 				
 				verseNum++;
 				verseRef = bookName +":"+ chapNum +":"+ verseNum;
@@ -119,26 +46,13 @@ public class Tokeniser extends ClassLoader{
 		}
 		
 		chapSc.close();
-			
+		return map;
 	}
 		
-	public static void main(String args[]){
+	public static void main(String args[]) throws FileNotFoundException{
 		Tokeniser toke= new Tokeniser();
-		try {
-			Scanner bigScan = new Scanner(new File("src/textDocs/fileDir.txt"));
-		} catch (FileNotFoundException e1) {
-			System.out.println("FIle DIrectory not found");
-		}
-		bigScan.useDelimiter("\n");
-		
-		File file =  new File("src/textDocs/Genesis.txt");
-		try {
-			toke.loadtoo(file, "Samuel1");
-		} catch (FileNotFoundException e) {
-			System.out.println("FILE NOT FOUND");
-		}
-	
-	
+		File file = new File("src/textDocs/Genesis.txt");
+		toke.loadToo(file, "Genesis");
 	}
 	
 }
